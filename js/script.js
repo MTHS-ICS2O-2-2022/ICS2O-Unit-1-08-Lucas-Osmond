9cdf4b1aeb5d0d1c2e282a581934828b77e1f968
Patrick Coxall <mr.coxall@mths.ca>
2023-05-24 08:10:36 -0400

    test

--- js/script.js
+++ js/script.js
@@ -4,6 +4,16 @@
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
 
+"use strict"
+
+/**
+ * Check servie worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-Unit-1-08-Lucas-Osmond/sw.js", {
+    scope: "/ICS2O-Unit-1-08-Lucas-Osmond/",
+  })
+}
 
 function myButtonClicked() {
   alert("hello, world!")
